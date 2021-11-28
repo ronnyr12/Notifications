8839bed3288478050f3a1f05503a60d54652f599
Ronny <ronny761@hityash.org>
2021-11-28 13:43:23 +0200

    service and thread class

--- app/src/main/java/com/example/notifications/FirstService.java
+++ app/src/main/java/com/example/notifications/FirstService.java
@@ -3,14 +3,37 @@ package com.example.notifications;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.widget.Toast;
 
 public class FirstService extends Service {
-    public FirstService() {
+    FirstThread firstThread;
+    String service_name = FirstService.class.getSimpleName();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Toast.makeText(this, "service oncreate: "+service_name, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        firstThread = null;
+        Toast.makeText(this, "service ondestroy: "+service_name, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "service onStartCommand: "+ service_name, Toast.LENGTH_SHORT).show();
+        if(firstThread==null){
+            firstThread = new FirstThread();
+            firstThread.start();
+        }
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
 }
--- app/src/main/java/com/example/notifications/FirstThread.java
+++ app/src/main/java/com/example/notifications/FirstThread.java
@@ -1,5 +1,23 @@
 package com.example.notifications;
 
-public class FirstThread extends  Thread{
+import android.util.Log;
 
+public class FirstThread extends Thread{
+    int counter = 30;
+
+    @Override
+    public void run() {
+        super.run();
+        while(counter>0){
+            try {
+                Thread.sleep(1000);
+                Log.d("service", "timer:" + counter);
+                counter--;
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        //stop the tread when getting to 0
+        stop();
+    }
 }
