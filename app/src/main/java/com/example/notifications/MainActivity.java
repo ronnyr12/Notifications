4c0e71af5f0c403f97ab828d09f6452aa0672cf2
Ronny <ronny761@hityash.org>
2021-11-28 14:38:35 +0200

    add music file to new raw directory

--- app/src/main/java/com/example/notifications/MainActivity.java
+++ app/src/main/java/com/example/notifications/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.notifications;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn_start, btn_stop;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_start = findViewById(R.id.btn_start);
+        btn_stop = findViewById(R.id.btn_stop);
+        btn_start.setOnClickListener(this);
+        btn_stop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(btn_start == view){
+            intent = new Intent(MainActivity.this,
+                    FirstService.class);
+            startService(intent);
+        }
+        if(btn_stop == view){
+            intent = new Intent(MainActivity.this,
+                    FirstService.class);
+            stopService(intent);
+        }
     }
 }
